67a96d9a687604d6cbfabe2152fa1a294c495f7d
pia-digiteer <pia.digiteer@gmail.com>
2025-09-26 16:46:39 +0800

    fix: removed duplicate import for scripts in view app layout

--- app/javascript/controllers/test_controller.js
+++ app/javascript/controllers/test_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("stimulus test")
+    console.log("stimulus test", this.element)
   }
 }
